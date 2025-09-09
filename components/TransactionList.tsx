6c7cca5b36ce8253f03f9dab19d79a34336c574f
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-09 17:58:40 +0300

    Delete components directory

--- components/TransactionList.tsx
+++
@@ -1,9 +0,0 @@
-<div
-  className={
-    `font-extrabold tabular-nums ${
-      ['expense','transfer-out'].includes(t.type) ? 'text-red-600' : 'text-green-700'
-    }`
-  }
->
-  {['expense','transfer-out'].includes(t.type) ? '-' : '+'}{t.amount.toLocaleString('ar-SA')} ر.س
-</div>
